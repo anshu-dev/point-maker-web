ebb26255dcee64e6f7a3cbdd08e4069345f94ef1
Anshu <anshu902kumar@gmail.com>
2021-05-12 14:45:01 +0530

    Added point compnent and removed bs-form

---
+++ app/components/point.js
@@ -0,0 +1,36 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+
+export default class PointsComponent extends Component {
+
+  @service store;
+  @tracked showEditPopup = false;
+  @tracked pointFields = {id: 0, name: ''};
+
+  @action
+    editPopup(point) {
+      this.showEditPopup = true;
+      const { id, name } = point;
+      this.pointFields = {id, name };
+    }
+
+  @action
+    async editPoint(pointData) {
+      let point = await this.store.findRecord('point', pointData.id);
+      point.name = pointData.name;
+      await point.save();
+      this.args.updatePoints();
+      this.showEditPopup = false;
+    }
+
+
+  @action
+    async deletePoint(id) {
+      let point = await this.store.findRecord('point', id, { reload: true });
+      await point.destroyRecord();
+
+      this.args.updatePoints();
+    }
+}
